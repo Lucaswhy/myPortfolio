7d40950f3c673c122e29c8e41567d202935b6156
unknown <lucas_herculano_2010@hotmail.com>
2021-08-07 16:11:16 -0300

    new skills page test

--- src/components/en-us/habilidade/habilidade-en.vue
+++ src/components/en-us/habilidade/habilidade-en.vue
@@ -1,64 +1,124 @@
 <template>
   <div class="container mt-4">
       <h2>Skills</h2>
-
-      <div class="div__main">
-          <div>
-              <img src="@/assets/logo.png" alt="">
-              <div class="div__text">
-                  <h3>Vue.js</h3>
-                  <span>I studied Vue.js for a long time, and worked in projects with him, I know how to utilize very well, I costume to use vuex to pages with states of data, and vue-route to single-pages. Also, I can use v-on, v-bind, filtering, the basic of vue in general. I have experience and I can do any project with him.</span>
-              </div>
-          </div>
-          <div>
-              <img src="@/assets/css3.png" alt="">
-              <div class="div__text">
-                  <h3>CSS3 + Sass</h3>
-                  <span>Experiences with various projects, in addition courses, I learned to use CSS3 and eventually learned Sass, like  this site  the two of them was used. I know how to use them with anything, position, flexbox, animations, and Sass to minify code in addition to be more legible to herance.</span>
-              </div>
-          </div>
-          <div>
-              <img src="@/assets/node.png" alt="">
-              <div class="div__text">
-                  <h3>Node.js</h3>
-                  <span>I study node.js since my university, was the technology I used to my TCC, and i realized how much I love node.js to back-end, i like so much Javascript and to use him freely with Node.js. I know how to utilize Express.js with another NPMs like Sequelize to SQL, or Mongoose to MongoDB(noSQL), or anything that can be necessary.</span>
-              </div>
-          </div>
-          <div>
-              <img src="@/assets/netcore.png" alt="">
-              <div class="div__text">
-                  <h3>.NET Core + EF</h3>
-                  <span>I did a course with Entity Framework and .NET Core where I developed an e-commerce(by my self), adding things like, pagination, filtering, CRUD, etc. For being a technology in my work space, I learned somethings off side too, and for that I can use very well.</span>
-              </div>
-          </div>
-          <div>
-              <img style="border-radius: 20px; margin-left: 0;" src="@/assets/selenium.png" alt="">
-              <div class="div__text">
-                  <h3>Selenium</h3>
-                  <span>Working with QA, i learned how to use Selenium with C# and in the IDE extension tool, which I use to make automation tests and help me to verify a task, discover a new scenario, or register new repetitive content to some occasions. Credits to Diego Molina for the logo.</span>
-              </div>
-          </div>
-          <div>
-              <a href="https://www.mysql.com"><img style="margin-top: 10px; border: none" src="@/assets/mysql.png" alt=""></a>
-              <div class="div__text">
-                  <h3>MySQL</h3>
-                  <span>I know how to utilize database SQL for applications, like the MySQL, integrating with any back-end. I already developed applications that was necessary to use procs, functions, I can use without any problem.</span>
-              </div>
-          </div>
-          <div>
-              <div class="div__text">
-                  <h3>NoSQL</h3>
-                  <span>I can use NoSQL databases without any problem, my TCC was done in MongoDB where I was realized that is easily and practical, i can create any system with NoSQL databases.</span>
-              </div>
-          </div>
-          <div>
-              <div class="div__text">
-                  <h3>Scrum + QA</h3>
-                  <span>Derived too from my QA experience, I have a good vision to system with quality and experience to final user. Always utilized Scrum to reunions with the team.  Never had problems with, I’m good with sprints and daily.</span>
-              </div>
-          </div>
+    
+    <div class="accordion" id="accordionExample">
+    
+  <div class="accordion-item mt-4">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+        Vue.js
+      <i class="bi bi-arrow-down-square-fill icon__arrow"></i>
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" >
+      <div class="accordion-body">
+          <span class="span__text">I studied Vue.js for a long time, and worked in projects with him, I know how to utilize very well, I costume to use vuex to pages with states of data, and vue-route to single-pages. Also, I can use v-on, v-bind, filtering, the basic of vue in general. I have experience and I can do any project with him.</span>
       </div>
+    </div>
   </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        <div style="background-color: blue;" class="box"></div>
+        CSS3 + Sass
+      <i class="bi bi-arrow-down-square-fill icon__arrow"></i>
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" >
+      <div class="accordion-body">
+          <span class="span__text">Experiences with various projects, in addition courses, I learned to use CSS3 and eventually learned Sass, like this site the two of them was used. I know how to use them with anything, position, flexbox, animations, and Sass to minify code in addition to be more legible to herance.</span>
+      </div>
+    </div>
+  </div>
+  
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingThree">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        Node.js
+        <i class="bi bi-arrow-down-square-fill icon__arrow"></i>
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" >
+      <div class="accordion-body">
+          <span class="span__text">I study node.js since my university, was the technology I used to my TCC, and i realized how much I love node.js to back-end, i like so much Javascript and to use him freely with Node.js. I know how to utilize Express.js with another NPMs like Sequelize to SQL, or Mongoose to MongoDB(noSQL), or anything that can be necessary.</span>
+      </div>
+    </div>
+  </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingFour">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+        .NET Core + EF
+        <i class="bi bi-arrow-down-square-fill icon__arrow"></i>
+      </button>
+    </h2>
+    <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" >
+      <div class="accordion-body">
+          <span class="span__text">I did a course with Entity Framework and .NET Core where I developed an e-commerce(by my self), adding things like, pagination, filtering, CRUD, etc. For being a technology in my work space, I learned somethings off side too, and for that I can use very well.</span>
+      </div>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingFive">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+        Selenium
+        <i class="bi bi-arrow-down-square-fill icon__arrow"></i>
+      </button>
+    </h2>
+    <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" >
+      <div class="accordion-body">
+          <span class="span__text">Working with QA, i learned how to use Selenium with C# and in the IDE extension tool, which I use to make automation tests and help me to verify a task, discover a new scenario, or register new repetitive content to some occasions. Credits to Diego Molina for the logo.</span>
+      </div>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingSix">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+        MySQL
+        <i class="bi bi-arrow-down-square-fill icon__arrow"></i>
+      </button>
+    </h2> 
+    <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix" >
+      <div class="accordion-body">
+          <span class="span__text">I know how to utilize database SQL for applications, like the MySQL, integrating with any back-end. I already developed applications that was necessary to use procs, functions, I can use without any problem.</span>
+      </div>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingSeven">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+        NoSQL
+        <i class="bi bi-arrow-down-square-fill icon__arrow"></i>
+      </button>
+    </h2>
+    <div id="collapseSeven" class="accordion-collapse collapse" aria-labelledby="headingSeven" >
+      <div class="accordion-body">
+          <span class="span__text">I can use NoSQL databases without any problem, my TCC was done in MongoDB where I was realized that is easily and practical, i can create any system with NoSQL databases.</span>
+      </div>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingEight">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+        Scrum + QA
+        <i class="bi bi-arrow-down-square-fill icon__arrow"></i>
+      </button>
+    </h2>
+    <div id="collapseEight" class="accordion-collapse collapse" aria-labelledby="headingEight" >
+      <div class="accordion-body">
+          <span class="span__text">Derived too from my QA experience, I have a good vision to system with quality and experience to final user. Always utilized Scrum to reunions with the team. Never had problems with, I’m good with sprints and daily.</span>
+      </div>
+    </div>
+  </div>
+
+    </div>
 </template>
 
 <script>
--- src/components/pt-br/habilidade/habilidade.vue
+++ src/components/pt-br/habilidade/habilidade.vue
@@ -2,63 +2,125 @@
   <div class="container mt-4">
       <h2>Habilidades</h2>
 
-      <div class="div__main">
-          <div>
-              <img src="@/assets/logo.png" alt="">
-              <div class="div__text">
-                  <h3>Vue.js</h3>
-                  <span>Estudei Vue.js por um bom tempo e realizei projetos com ele, sei utilizar muito bem, costumo utilizar muito Vuex para páginas com estado de dados e Vue-route para lidar bem com single pages. Além de conseguir utilizar v-on, v-bind, filtering, o básico em do vue em geral. Tenho experiência e consigo realizar qualquer projeto nele.</span>
-              </div>
-          </div>
-          <div>
-              <img src="@/assets/css3.png" alt="">
-              <div class="div__text">
-                  <h3>CSS3 + Sass</h3>
-                  <span>Diversas experiências, além de cursos realizados, eu aprendi a mexer com CSS3 e eventuamente aprendi a mexer com o Sass, no qual ambos foram utilizados nesse site. Sei lidar com qualquer coisa, position, flexbox, animações, e utilizo o Sass para minificar além de ficar mais legível a herança.</span>
-              </div>
-          </div>
-          <div>
-              <img src="@/assets/node.png" alt="">
-              <div class="div__text">
-                  <h3>Node.js</h3>
-                  <span>Estudo Node.js desde a faculdade, onde utilizei para fazer meu TCC e descobri como gostava dessa tecnologia para o back-end, gosto muito de Javascript e de poder utilizar ele livremente com o Node.js. Sei utilizar muito bem o Express.js junto com outros npms como Sequelize para SQL ou Mongoose para MongoDB (noSQL), ou qualquer outro que seja necessário.</span>
-              </div>
-          </div>
-          <div>
-              <img src="@/assets/netcore.png" alt="">
-              <div class="div__text">
-                  <h3>.NET Core + EF</h3>
-                  <span>Fiz um curso com o Entity Framework e .NET Core onde eu desenvolvi um pequeno e-commerce(por mim mesmo), adicionando coisas como paginação, filtragem, CRUD, etc. Por ser a tecnologia em ambiente de trabalho, eu aprendi algumas coisas por fora também, eu consigo lidar muito bem. </span>
-              </div>
-          </div>
-          <div>
-              <img style="border-radius: 20px" src="@/assets/selenium.png" alt="">
-              <div class="div__text">
-                  <h3>Selenium</h3>
-                  <span>Trabalhando como QA eu aprendi mexer com Selenium no C# e também utilizando sua ferramenta de extensão IDE, na qual permita que eu faça testes automatizados e me ajude na validação de uma tarefa, no descobrimento de um novo cenário, ou então no cadastro de conteúdo repetido para certas ocasiões. <a style="font-size: 12px;" href="https://commons.wikimedia.org/wiki/File:Selenium_Logo.png">Crédito do logo ao Diego Molina.</a> </span>
-              </div>
-          </div>
-          <div>
-              <a href="https://www.mysql.com"><img style="margin-top: 10px; border: none" src="@/assets/mysql.png" alt=""></a>
-              <div class="div__text">
-                  <h3>MySQL</h3>
-                  <span>Sei utilizar bancos de dados SQL para aplicações, como o MySQL, integrando tranquilamente com qualquer back-end. Já desenvolvi aplicações que foram necessário o uso de Procs, functions, e consigo utilizar sem nenhum problema.</span>
-              </div>
-          </div>
-          <div>
-              <div class="div__text">
-                  <h3>NoSQL</h3>
-                  <span>Consigo utilizar databases NoSQL sem problema algum, meu TCC foi feito em MongoDB no qual achei bem prático e fácil de utilizar, consigo fazer qualquer sistema com NoSQL. </span>
-              </div>
-          </div>
-          <div>
-              <div class="div__text">
-                  <h3>Scrum + QA</h3>
-                  <span>Também derivado da minha experiência de QA, tenho uma boa visão de um sistema com qualidade e visando sempre a experiência do usuário final. Utilizei muito a metodologia Scrum para reuniões com o time e sempre foi um sucesso, lido bem com as sprints e as dailys sendo bem comunicativo.</span>
-              </div>
-          </div>
+<div class="div__main">
+  <div class="accordion" id="accordionExample">
+    
+  <div class="accordion-item mt-4">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+        Vue.js
+      <i class="bi bi-arrow-down-square-fill icon__arrow"></i>
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" >
+      <div class="accordion-body">
+          <span class="span__text">Estudei Vue.js por um bom tempo e realizei projetos com ele, sei utilizar muito bem, costumo utilizar muito Vuex para páginas com estado de dados e Vue-route para lidar bem com single pages. Além de conseguir utilizar v-on, v-bind, filtering, o básico em do vue em geral. Tenho experiência e consigo realizar qualquer projeto nele.</span>
       </div>
+    </div>
   </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        <div style="background-color: blue;" class="box"></div>
+        CSS3 + Sass
+      <i class="bi bi-arrow-down-square-fill icon__arrow"></i>
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" >
+      <div class="accordion-body">
+        <span class="span__text">Diversas experiências, além de cursos realizados, eu aprendi a mexer com CSS3 e eventuamente aprendi a mexer com o Sass, no qual ambos foram utilizados nesse site. Sei lidar com qualquer coisa, position, flexbox, animações, e utilizo o Sass para minificar além de ficar mais legível a herança.</span>
+      </div>
+    </div>
+  </div>
+  
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingThree">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        Node.js
+        <i class="bi bi-arrow-down-square-fill icon__arrow"></i>
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" >
+      <div class="accordion-body">
+        <span class="span__text">Estudo Node.js desde a faculdade, onde utilizei para fazer meu TCC e descobri como gostava dessa tecnologia para o back-end, gosto muito de Javascript e de poder utilizar ele livremente com o Node.js. Sei utilizar muito bem o Express.js junto com outros npms como Sequelize para SQL ou Mongoose para MongoDB (noSQL), ou qualquer outro que seja necessário.</span>
+      </div>
+    </div>
+  </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingFour">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+        .NET Core + EF
+        <i class="bi bi-arrow-down-square-fill icon__arrow"></i>
+      </button>
+    </h2>
+    <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" >
+      <div class="accordion-body">
+        <span class="span__text">Fiz um curso com o Entity Framework e .NET Core onde eu desenvolvi um pequeno e-commerce(por mim mesmo), adicionando coisas como paginação, filtragem, CRUD, etc. Por ser a tecnologia em ambiente de trabalho, eu aprendi algumas coisas por fora também, eu consigo lidar muito bem.</span>
+      </div>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingFive">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+        Selenium
+        <i class="bi bi-arrow-down-square-fill icon__arrow"></i>
+      </button>
+    </h2>
+    <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" >
+      <div class="accordion-body">
+        <span class="span__text">Trabalhando como QA eu aprendi mexer com Selenium no C# e também utilizando sua ferramenta de extensão IDE, na qual permita que eu faça testes automatizados e me ajude na validação de uma tarefa, no descobrimento de um novo cenário, ou então no cadastro de conteúdo repetido para certas ocasiões.</span>
+      </div>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingSix">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+        MySQL
+        <i class="bi bi-arrow-down-square-fill icon__arrow"></i>
+      </button>
+    </h2> 
+    <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix" >
+      <div class="accordion-body">
+        <span class="span__text">Sei utilizar bancos de dados SQL para aplicações, como o MySQL, integrando tranquilamente com qualquer back-end. Já desenvolvi aplicações que foram necessário o uso de Procs, functions, e consigo utilizar sem nenhum problema.</span>
+      </div>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingSeven">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+        NoSQL
+        <i class="bi bi-arrow-down-square-fill icon__arrow"></i>
+      </button>
+    </h2>
+    <div id="collapseSeven" class="accordion-collapse collapse" aria-labelledby="headingSeven" >
+      <div class="accordion-body">
+        <span class="span__text">Consigo utilizar databases NoSQL sem problema algum, meu TCC foi feito em MongoDB no qual achei bem prático e fácil de utilizar, consigo fazer qualquer sistema com NoSQL.</span>
+      </div>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingEight">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+        Scrum + QA
+        <i class="bi bi-arrow-down-square-fill icon__arrow"></i>
+      </button>
+    </h2>
+    <div id="collapseEight" class="accordion-collapse collapse" aria-labelledby="headingEight" >
+      <div class="accordion-body">
+        <span class="span__text">Também derivado da minha experiência de QA, tenho uma boa visão de um sistema com qualidade e visando sempre a experiência do usuário final. Utilizei muito a metodologia Scrum para reuniões com o time e sempre foi um sucesso, lido bem com as sprints e as dailys sendo bem comunicativo.</span>
+      </div>
+    </div>
+  </div>
+
+  </div>
+</div>
 </template>
 
 <script>
